1a4f8ca4132eaf38625c735bb7d3b17893038f4b
Binaya Dhakal <binaydhakal35@gmail.com>
2021-05-18 12:55:09 +0545

    added to next branch

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ import PlacesAutocomplete, {
   geocodeByPlaceId,
   getLatLng,
 } from "react-places-autocomplete";
-
+//here changed in googlemapper branch
 function Search() {
   const [address, setAddress] = useState("");
   const [coordinates, setCoordinates] = useState({
